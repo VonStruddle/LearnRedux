e79f604fbc1dcaea16e40af67a023bc535957f64
VonStruddle <quentin.durantay@gmail.com>
2016-10-25 16:03:59 +0200

    Add store and reducer

---
+++ app/redux-example-todos.jsx
@@ -0,0 +1,17 @@
+var redux = require('redux');
+
+console.log('Starting redux-todo example');
+
+var reducer = (state = {
+  searchText: '',
+  showCompleted: false,
+  todos: [] 
+}, action) => {
+  return state;
+};
+
+var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+
+console.log('currentState: ', currentState)
